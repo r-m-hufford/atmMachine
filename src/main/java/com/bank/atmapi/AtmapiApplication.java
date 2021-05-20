6014657dc44b0364f145e444790c25e55220b8b6
Ryan M Hufford <ryanmhufford@Ryans-MacBook-Pro.local>
2021-05-20 11:17:33 -0400

    checkpoint

---
+++ src/main/java/com/bank/atmapi/AtmapiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.atmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/atmapi/AtmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.atmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
